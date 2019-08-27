9acd53556fc36fce3a0d14288c868b3115d5dca4
Jorge <jorge@krealo.pe>
2019-08-27 15:22:00 -0500

    jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,7 +14,8 @@ node {
     }
     stage('Deploy') {
         withCredentials([file(credentialsId: 'firebase-credentials', variable: 'FIREBASE_CREDENTIALS')]) {
-            sh "sed 's|FBC_SRC|'$FIREBASE_CREDENTIALS'|g' Dockerfile.tpl > Dockerfile"
+            sh "cat $FIREBASE_CREDENTIALS > src/main/resources/firebase.json"
+//            sh "sed 's|FBC_SRC|'$FIREBASE_CREDENTIALS'|g' Dockerfile.tpl > Dockerfile"
             sh 'sudo docker build -t jaxkodex/challenge-backend:1.0 .'
             sh 'sudo docker push jaxkodex/challenge-backend:1.0'
 //            sh 'docker run -d challenge:1.0'
