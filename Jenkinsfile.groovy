d2aaa52f0acf6ffa4c948d14d701b81c866f44b5
Jorge <jorge@krealo.pe>
2019-08-26 21:40:12 -0500

    fix typo

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,13 +3,14 @@ node {
         checkout scm
     }
     stage('Unit & Integration Tests') {
-        withCredentials([file(credentialsID: 'firebase-credentials', variable: 'FILE')]) {
+        withCredentials([file(credentialsId: 'firebase-credentials', variable: 'FILE')]) {
+            sh 'echo $FILE'
             sh 'cp $FILE ./src/main/resources/'
             sh './gradlew clean test --no-daemon'
         }
     }
     stage('Build') {
-        withCredentials([file(credentialsID: 'firebase-credentials', variable: 'FILE')]) {
+        withCredentials([file(credentialsId: 'firebase-credentials', variable: 'FILE')]) {
             sh 'cp $FILE ./src/main/resources/'
             sh './gradlew build --no-daemon'
         }
